bdb437921cabe8dc03125603c66d5f46cf815036
Sanjeeb Kumar Sahoo <50053860+blackheart2009@users.noreply.github.com>
2019-05-15 07:34:28 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  apiKey: '',
+  discordToken: 'NTczMzUyNjMyNDcxNTE5MjMy.XNtxGg.Rh46b1GRPRhIqsaRKDLgANhneKE',
+  clans: [
+    {
+      tag: '#L2RJC0CV',
+      channelID: '573729080604622898',
+      color: 3447003
+    }
+  ],
+  timeDelay: 90,
+  showLeague: false,
+  useRichEmbed: true
+};
