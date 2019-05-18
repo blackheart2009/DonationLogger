74ce4df5791ef5ec4e3f5c2535f6787454f6a72f
Sanjeeb Kumar Sahoo <50053860+blackheart2009@users.noreply.github.com>
2019-05-18 14:10:22 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 module.exports = {
-  apiKey: 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6ImNmZTA5ZmI2LWI5ZWUtNGRkNS05ZjhiLWM3YTZkYmRmZGM3OSIsImlhdCI6MTU1ODE2ODM5NCwic3ViIjoiZGV2ZWxvcGVyLzBmOTM4YTBmLTE3ZjAtMjNjYS03MDE1LWZkZjhmMmM0NTE5ZSIsInNjb3BlcyI6WyJjbGFzaCJdLCJsaW1pdHMiOlt7InRpZXIiOiJkZXZlbG9wZXIvc2lsdmVyIiwidHlwZSI6InRocm90dGxpbmcifSx7ImNpZHJzIjpbIjEzLjIzMi4xNS4yMDUiXSwidHlwZSI6ImNsaWVudCJ9XX0.6BwxyKGMBj2ksnw65F3a2Bytt57PIV676RyoxDO3MhtO2prS7q3qRx_6F7VbXYlK9LOtOzwiJffK6RB6lpALWQ',
-  discordToken: 'NTczMzUyNjMyNDcxNTE5MjMy.XNtxGg.Rh46b1GRPRhIqsaRKDLgANhneKE',
+  apiKey: '',
+  discordToken: '',
   clans: [
     {
-      tag: '#L2RJC0CV',
-      channelID: '573729080604622898',
+      tag: '',
+      channelID: '',
       color: 3447003
     }
   ],
