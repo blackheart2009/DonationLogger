cf16a393db3aea368951124b8dc8437afcea8af6
Sanjeeb Kumar Sahoo <50053860+blackheart2009@users.noreply.github.com>
2019-05-27 13:16:15 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,14 +1,24 @@
 module.exports = {
-  apiKey: '',
-  discordToken: '',
-  clans: [
+  "apiKey": '',
+  "discordToken": '',
+  "clans": [
     {
-      tag: '',
-      channelID: '',
-      color: 3447003
+      "tag": '#L2RJC0CV',
+      "channelID": '573729080604622898',
+      "color": 3447003
+    },
+    {
+      "tag": '#9QL9Y002',
+      "channelID": '582465897017638912',
+      "color": 1127128
+    },
+    {
+      "tag": '#UJPLQJ9G',
+      "channelID": '5824659433593856',
+      "color": 14177041
     }
   ],
   timeDelay: 90,
-  showLeague: false,
+  showLeague: true,
   useRichEmbed: true
 };
