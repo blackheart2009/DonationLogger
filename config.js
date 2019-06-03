ee62efa094939b74566ae9989c6ee6a1b8368ce5
Sanjeeb Kumar Sahoo <50053860+blackheart2009@users.noreply.github.com>
2019-06-03 20:08:37 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,16 +6,6 @@ module.exports = {
       "tag": '#L2RJC0CV',
       "channelID": '573729080604622898',
       "color": 3447003
-    },
-    {
-      "tag": '#9QL9Y002',
-      "channelID": '582465897017638912',
-      "color": 1127128
-    },
-    {
-      "tag": '#UJPLQJ9G',
-      "channelID": '5824659433593856',
-      "color": 14177041
     }
   ],
   timeDelay: 90,
